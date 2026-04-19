59ebbe6ca071694722228ac0a39ff4c6cd58099e
DioGenAsIs <neoboy773@gmail.com>
2026-04-19 22:15:21 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,15 +6,5 @@ const nextConfig = {
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "tengimarket.kz" }],
-        destination: "https://www.tengimarket.kz/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 export default nextConfig;
