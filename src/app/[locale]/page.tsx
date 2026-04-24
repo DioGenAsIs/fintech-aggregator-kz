1c338a97861caf93a70024d2dd511aa61428fae9
DioGenAsIs <neoboy773@gmail.com>
2026-04-24 15:13:48 +0500

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,16 +1,8 @@
 import offers from "@/data/offers.json";
 import { getMessages } from "@/lib/messages";
-import { isLocale, type Locale } from "@/lib/i18n";
-import { notFound } from "next/navigation";
 import { LandingPage } from "@/components/landing/LandingPage";
 
-export default function LocalePage({ params }: { params: { locale: string } }) {
-  if (!isLocale(params.locale)) {
-    notFound();
-  }
-
-  const locale = params.locale as Locale;
-  const messages = getMessages(locale);
-
-  return <LandingPage locale={locale} messages={messages} offers={offers} />;
+export default function RootPage() {
+  const messages = getMessages("kk");
+  return <LandingPage locale="kk" messages={messages} offers={offers} />;
 }
